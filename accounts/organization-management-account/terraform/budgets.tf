76315567702891b804aed24f97fef880df24e897
Edwin Moedano <edwin.moedano@caylent.com>
2026-03-31 17:54:50 -0600

    feat: budgets module defined for management account

---
+++ accounts/organization-management-account/terraform/budgets.tf
@@ -0,0 +1,22 @@
+module "budgets" {
+  source = "../../../modules/terraform-aws-budgets"
+  budgets = [
+    {
+      name         = "General Monthly Budget"
+      budget_type  = "COST"
+      limit_amount = 7000
+      time_unit    = "MONTHLY"
+      account_id   = local.account_map["organization_management_account"]
+
+      notification = [
+        {
+          comparison_operator        = "GREATER_THAN"
+          threshold                  = 80
+          threshold_type             = "PERCENTAGE"
+          notification_type          = "FORECASTED"
+          subscriber_email_addresses = ["aws.billing@sentientdecisionscience.com"]
+        }
+      ]
+    }
+  ]
+}

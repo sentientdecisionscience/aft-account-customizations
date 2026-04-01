557016fbd18707b6462cdcd675caee0af8a5fe0d
Edwin Moedano <edwin.moedano@caylent.com>
2026-04-01 10:47:02 -0600

    fix: local variable defined

--- accounts/shared-services-account/terraform/permission_sets.tf
+++ accounts/shared-services-account/terraform/permission_sets.tf
@@ -1,7 +1,7 @@
 # Administrator Permission Set
 resource "aws_ssoadmin_permission_set" "admin" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  name             = "${permission_sets_prefix}AdministratorAccess"
+  name             = "${local.permission_sets_prefix}AdministratorAccess"
   description      = "Provides full administrative access"
   session_duration = "PT8H"
 }
@@ -15,7 +15,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "admin_policy" {
 # Billing Permission Set
 resource "aws_ssoadmin_permission_set" "billing" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  name             = "${permission_sets_prefix}Billing"
+  name             = "${local.permission_sets_prefix}Billing"
   description      = "Grants permissions for billing and cost management. This includes viewing account usage and viewing and modifying budgets and payment methods."
   session_duration = "PT8H"
 }
@@ -29,7 +29,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "billing_policy" {
 # Power User Permission Set
 resource "aws_ssoadmin_permission_set" "power_user" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  name             = "${permission_sets_prefix}PowerUserAccess"
+  name             = "${local.permission_sets_prefix}PowerUserAccess"
   description      = "Provides full access to AWS services and resources, but does not allow management of Users and groups."
   session_duration = "PT8H"
 }
@@ -43,7 +43,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "power_user_policy" {
 # Network Administrator Permission Set
 resource "aws_ssoadmin_permission_set" "network_administrator" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  name             = "${permission_sets_prefix}NetworkAdministrator"
+  name             = "${local.permission_sets_prefix}NetworkAdministrator"
   description      = "Grants full access permissions to AWS services and actions required to set up and configure AWS network resources."
   session_duration = "PT8H"
 }
@@ -57,7 +57,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "network_administrator_policy"
 # Database Administrator Permission Set
 resource "aws_ssoadmin_permission_set" "database_admin" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  name             = "${permission_sets_prefix}DatabaseAdministrator"
+  name             = "${local.permission_sets_prefix}DatabaseAdministrator"
   description      = "Grants full access permissions to AWS services and actions required to set up and configure AWS database services."
   session_duration = "PT8H"
 }
@@ -71,7 +71,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "database_admin_policy" {
 # Security Audit Permission Set
 resource "aws_ssoadmin_permission_set" "security_audit" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  name             = "${permission_sets_prefix}SecurityAudit"
+  name             = "${local.permission_sets_prefix}SecurityAudit"
   description      = "The security audit template grants access to read security configuration metadata. It is useful for software that audits the configuration of an AWS account."
   session_duration = "PT8H"
 }
@@ -85,7 +85,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "security_audit_policy" {
 # SupportAccess Permission Set
 resource "aws_ssoadmin_permission_set" "support_access" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  name             = "${permission_sets_prefix}SupportAccess"
+  name             = "${local.permission_sets_prefix}SupportAccess"
   description      = "Allows users to access the AWS Support Center."
   session_duration = "PT8H"
 }
@@ -99,7 +99,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "support_access_policy" {
 # System Administrator Permission Set
 resource "aws_ssoadmin_permission_set" "system_admin" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  name             = "${permission_sets_prefix}SystemAdministrator"
+  name             = "${local.permission_sets_prefix}SystemAdministrator"
   description      = "Grants full access permissions necessary for resources required for application and development operations."
   session_duration = "PT8H"
 }
@@ -114,7 +114,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "system_admin_policy" {
 # Read Only  Permission Set
 resource "aws_ssoadmin_permission_set" "read_only" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  name             = "${permission_sets_prefix}ReadOnlyAccess"
+  name             = "${local.permission_sets_prefix}ReadOnlyAccess"
   description      = "Provides read-only access to AWS services and resources."
   session_duration = "PT8H"
 }

5d9708d6fb924c5fa0b40a06303aef3f79bca481
Edwin Moedano <edwin.moedano@caylent.com>
2026-04-01 09:15:54 -0600

    feat: enable guardduty delegated admin

--- accounts/organization-management-account/terraform/guardduty.tf
+++ accounts/organization-management-account/terraform/guardduty.tf
@@ -14,15 +14,15 @@
 
 # Delegate Administration of GuardDuty to the Audit Account
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/organizations_delegated_administrator
-# resource "aws_organizations_delegated_administrator" "guardduty_delegated_administrator" {
-#   account_id        = local.account_map["audit"]
-#   service_principal = "guardduty.amazonaws.com"
-# }
+resource "aws_organizations_delegated_administrator" "guardduty_delegated_administrator" {
+  account_id        = local.account_map["audit"]
+  service_principal = "guardduty.amazonaws.com"
+}
 
 # # Set the Audit Account as the Administrator of GuardDuty in the current (home) region
 # # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/guardduty_organization_admin_account
-# resource "aws_guardduty_organization_admin_account" "guardduty_admin_account" {
-#   depends_on = [aws_organizations_delegated_administrator.guardduty_delegated_administrator]
+resource "aws_guardduty_organization_admin_account" "guardduty_admin_account" {
+  depends_on = [aws_organizations_delegated_administrator.guardduty_delegated_administrator]
 
-#   admin_account_id = local.account_map["audit"]
-# }
+  admin_account_id = local.account_map["audit"]
+}

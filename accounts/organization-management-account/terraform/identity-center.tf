f005f1a59843d2c639065601d68393584757f3ff
Edwin Moedano <edwin.moedano@caylent.com>
2026-04-01 09:47:10 -0600

    feat: updated locals ou and accounts id and delegate identity center to shared services

---
+++ accounts/organization-management-account/terraform/identity-center.tf
@@ -0,0 +1,6 @@
+# Delegate SSO Identity Center to the Shared Services Account
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/organizations_delegated_administrator
+resource "aws_organizations_delegated_administrator" "sso_delegated_administrator" {
+  service_principal = "sso.amazonaws.com"
+  account_id        = local.account_map["shared_services_account"]
+}

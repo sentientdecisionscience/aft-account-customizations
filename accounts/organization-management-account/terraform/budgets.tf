7440cbf9afef34c4d12dd3a63cfe91fb0a71e4fe
Edwin Moedano <edwin.moedano@caylent.com>
2026-04-02 08:20:35 -0600

    feat: created important_budget alarm

--- accounts/organization-management-account/terraform/budgets.tf
+++ accounts/organization-management-account/terraform/budgets.tf
@@ -20,3 +20,26 @@ module "budgets" {
     }
   ]
 }
+
+module "important_budget" {
+  source = "../../../modules/terraform-aws-budgets"
+  budgets = [
+    {
+      name         = "Important Monthly Budget"
+      budget_type  = "COST"
+      limit_amount = 3000
+      time_unit    = "MONTHLY"
+      account_id   = local.account_map["organization_management"]
+
+      notification = [
+        {
+          comparison_operator        = "GREATER_THAN"
+          threshold                  = 80
+          threshold_type             = "PERCENTAGE"
+          notification_type          = "FORECASTED"
+          subscriber_email_addresses = local.budget_alarm_email_addresses
+        }
+      ]
+    }
+  ]
+}
